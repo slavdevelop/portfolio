ae3cdf462cea6a705a17dc25484c8ad8cd2514c6
Slavi Shtipkov <development@shtipkov.com>
2019-11-10 22:18:37 +0200

    Removed generated files and add 2 simple theme objects

--- packages/design-system/src/index.js
+++ packages/design-system/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div className="app">Application</div>,
+  document.getElementById('root')
+);
--- packages/design-system/stories/1-Button.stories.js
+++ packages/design-system/stories/1-Button.stories.js
@@ -3,10 +3,12 @@ import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
 
 export default {
-  title: 'Button',
+  title: 'Button'
 };
 
-export const text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+export const text = () => (
+  <Button onClick={action('clicked')}>Hello Button</Button>
+);
 
 export const emoji = () => (
   <Button onClick={action('clicked')}>
